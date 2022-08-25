a5de5fc8051532e9d4970bf23963616d5e2d5cb1
황지민 <80872558+krapoi@users.noreply.github.com>
2022-08-25 19:39:53 +0900

    commit

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.camera
 
+import android.Manifest
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.example.camera.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    lateinit var cameraPermission: ActivityResultLauncher<String>
+    lateinit var storagePermission: ActivityResultLauncher<String>
+    lateinit var cameraLauncher: ActivityResultLauncher<Uri>
+
+    var photoUri:Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                setViews()
+            } else {
+                Toast.makeText(baseContext, "권한을 승인해야 앱을 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+        cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                openCamera()
+            } else {
+                Toast.makeText(baseContext, "권한을 승인해야 카메라를 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            if (it) {
+                binding.imagePreview.setImageURI(photoUri)
+            }
+        }
+    }
+
+    fun setViews() {
+        binding.cameraBtn.setOnClickListener {
+            cameraPermission.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    fun openCamera() {
+        val photoFile = File.createTempFile(
+            "IMG_",
+        ".jpg",
+            getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
+
+        photoUri = FileProvider.getUriForFile(
+            this, "${packageName}.provider", photoFile
+        )
+
+        cameraLauncher.launch(photoUri)
     }
 }
